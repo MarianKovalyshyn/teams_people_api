86fb8a29f4ad5643591be04135e1e4e725513125
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-01-13 15:39:51 +0200

    Add views with CRUD operations for person & team

--- teams_people_service/views.py
+++ teams_people_service/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from teams_people_service.models import Person, Team
+from teams_people_service.serializers import PersonSerializer, TeamSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
