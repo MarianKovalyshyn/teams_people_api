49465ebad52f8eb054e8a2833b3db9c1edba77ee
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-01-14 23:52:46 +0200

    Delete comments. Fix some code style issues

--- teams_people_api/urls.py
+++ teams_people_api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('teams_people_service.urls', namespace='teams_people_service')),
+    path('', include(
+        'teams_people_service.urls', namespace='teams_people_service'
+    )),
 ]
--- teams_people_service/models.py
+++ teams_people_service/models.py
@@ -5,7 +5,9 @@ class Person(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
     email = models.EmailField(max_length=50, unique=True)
-    team = models.ForeignKey('Team', on_delete=models.SET_NULL, null=True, related_name='persons')
+    team = models.ForeignKey(
+        'Team', on_delete=models.SET_NULL, null=True, related_name='persons'
+    )
 
     def __str__(self):
         return f'{self.name} {self.surname}'
